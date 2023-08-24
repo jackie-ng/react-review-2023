dbbff932e8aa9061a35ce0dfd7c69a97072c5867
Trang Nguyen <ntttrang2130@gmail.com>
2023-08-24 17:09:30 -0400

    set up API router for POSTS

--- react-mysql/server/index.js
+++ react-mysql/server/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
+
+app.use(express.json());
+
 const db = require('./models')
+
+//Routers
+const postRouter = require('./routes/Posts')
+app.use("/posts", postRouter)
+// 'localhost:3000/posts'
 
 db.sequelize.sync().then(() => {
   app.listen(3000, () => {
