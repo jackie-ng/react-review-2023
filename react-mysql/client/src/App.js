dab53c839946471e5b2e45cfacecb9f3b46669a0
Trang Nguyen <ntttrang2130@gmail.com>
2023-08-24 21:54:45 -0400

    api integration

---
+++ react-mysql/client/src/App.js
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import './App.css';
+import axios from "axios";
+
+function App() {
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts").then((response) => {
+      console.log(response)
+    })
+  }, [])
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
