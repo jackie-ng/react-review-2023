7b1643eddc9d6ffa2b165d1faaaddbb7a45ebd0f
Trang Nguyen <ntttrang2130@gmail.com>
2023-08-24 21:54:06 -0400

    integrate cors

--- react-mysql/server/index.js
+++ react-mysql/server/index.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors')
 
 app.use(express.json());
-
+app.use(cors())
 const db = require('./models')
 
 //Routers
@@ -11,7 +12,7 @@ app.use("/posts", postRouter)
 // 'localhost:3000/posts'
 
 db.sequelize.sync().then(() => {
-  app.listen(3000, () => {
-    console.log("Server running on port 3000");
+  app.listen(3001, () => {
+    console.log("Server running on port 3001");
   });
 });
