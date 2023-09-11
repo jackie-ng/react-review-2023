949a4d129220d68cf9765db7218d4dbded01a16b
Trang Nguyen <ntttrang2130@gmail.com>
2023-09-11 13:43:10 -0400

    reset client with vite

--- project-management/server/index.js
+++ project-management/server/index.js
@@ -1,47 +1,22 @@
-import express from "express"
-import mysql from "mysql"
-const app = express();
-
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "projectmgr"
-})
-
+import express from "express";
+import postRoutes from "./routes/posts.js"
+import cors from "cors";
 // If there's an auth problem
 // ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
-
 // Express server middleware
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.use(express.json())
+app.use("/api/posts", postRoutes)
 
-app.get("/", (req, res) => {
-  res.json("Hello this is the backend")
-})
 
-app.get("/books", (req, res) => {
-  const q = "SELECT * FROM books"
-  db.query(q, (err, data) => {
-    if (err) return res.json(err)
-    return res.json(data)
-  })
-})
 
-app.post("/books", (req, res) => {
-  const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)"
-  const values = [
-    req.body.title,
-    req.body.desc,
-    req.body.cover
-  ]
+app.get("/test", (req, res) => {
+  res.json("hello");
+});
 
-  db.query(q, [values], (err, data) => {
-    if (err) return res.json(err);
-    return res.json("Book has been created successfully")
-  })
-})
 
 app.listen(8800, () => {
-  console.log("Connected to backend");
-})
+  console.log("Connected to backend.");
+});
