ac137049f13bafef5e6ad4f5600f6d46c10f34cd
Trang Nguyen <ntttrang2130@gmail.com>
2023-09-13 14:59:58 -0400

    add bcrypt

--- project-management/server/controllers/auth.js
+++ project-management/server/controllers/auth.js
@@ -1,9 +1,15 @@
-import { db } from '../db.js'
+import { db } from '../db.js';
+import bcrypt from "bcryptjs";
+
 export const register = (req, res) => {
   // CHECK EXISTING USER
   const q = "SELECT * FROM users WHERE email = ? OR username = ?"
   db.query(q, [req.body.email], req.body.username, (err, data) => {
     if (err) return res.json(err)
+    if (data.length) return res.status(409).json("User already exists");
+    // HASH THE PASSWORD AND CREATE A USER
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt)
   })
 }
 
