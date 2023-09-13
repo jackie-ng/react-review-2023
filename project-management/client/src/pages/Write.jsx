e6135a4c11dde5800b1f165ee74b8930fc1b4955
Trang Nguyen <ntttrang2130@gmail.com>
2023-09-12 23:49:05 -0400

    add react-quill

--- project-management/client/src/pages/Write.jsx
+++ project-management/client/src/pages/Write.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 export default function Write() {
   return (
-    <div>Write</div>
+    <div className="add">
+      <div className="content">
+        <input type="text" placeholder='title' />
+        <div className="editorContainer">
+          <ReactQuill
+            className="editor"
+            theme="snow"
+          // value={value}
+          // onChange={setValue}
+          />
+        </div>
+      </div>
+      <div className="menu">
+        <div className="item">item 1</div>
+        <div className="item">item 2 </div>
+      </div>
+    </div>
   )
 }
