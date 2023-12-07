6dff2e760b7459f1e31fed1de3346b7c6cfef39c
Trang Nguyen <ntttrang2130@gmail.com>
2023-12-07 16:23:48 -0500

    add handleChange for register page

--- social-network-react-sql/client/src/pages/register/Register.jsx
+++ social-network-react-sql/client/src/pages/register/Register.jsx
@@ -1,7 +1,22 @@
 import { Link } from "react-router-dom";
 import "./register.scss";
+import { useState } from "react";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+    name: ""
+  })
+
+  const handleChange = e => {
+    setInputs(prev => ({
+      ...prev,
+      [e.target.name]: e.target.value
+    }))
+  }
+
   return (
     <div className="register">
       <div className="card">
@@ -14,16 +29,16 @@ const Register = () => {
           </p>
           <span>Do you have an account?</span>
           <Link to="/login">
-          <button>Login</button>
+            <button>Login</button>
           </Link>
         </div>
         <div className="right">
           <h1>Register</h1>
           <form>
-            <input type="text" placeholder="Username" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Username" name="username" onChange={handleChange} />
+            <input type="email" placeholder="Email" name="email" onChange={handleChange} />
+            <input type="password" placeholder="Password" name="password" onChange={handleChange} />
+            <input type="text" placeholder="Name" name="name" onChange={handleChange} />
             <button>Register</button>
           </form>
         </div>
