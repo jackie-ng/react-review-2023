d6438f6d4c02fd6cc4bcdd4af6985c401202e0d3
Trang Nguyen <ntttrang2130@gmail.com>
2023-12-06 22:31:43 -0500

    initial commit

---
+++ social-network-react-sql/api/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+import userRoutes from "./routes/users.js"
+
+app.use("/api/users", userRoutes)
+
+app.listen(8800, () => {
+  console.log("API working!");
+})
