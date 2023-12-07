386069a88644a67d36f6910424eb0f2da6c8755d
Trang Nguyen <ntttrang2130@gmail.com>
2023-12-07 15:51:09 -0500

    add logout backend

--- social-network-react-sql/api/controllers/auth.js
+++ social-network-react-sql/api/controllers/auth.js
@@ -58,6 +58,10 @@ export const login = (req, res) => {
     }).status(200).json(others);
   });
 };
-export const logout = (req, res) => {
 
+export const logout = (req, res) => {
+  res.clearCookie("accessToken", {
+    secure: true,
+    sameSite: "none"
+  }).status(200).json("User has been logged out")
 }
