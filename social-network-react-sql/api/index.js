f0a00f64ffd65b27dcfecee07a06ed9c4d1c004e
Trang Nguyen <ntttrang2130@gmail.com>
2023-12-07 19:00:18 -0500

    update index.js

--- social-network-react-sql/api/index.js
+++ social-network-react-sql/api/index.js
@@ -11,8 +11,14 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 
 // middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true)
+  next()
+})
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:3000"
+}))
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
