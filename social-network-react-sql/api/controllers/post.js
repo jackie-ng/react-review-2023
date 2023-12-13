e11df3da83696b35e546f89bf25fd7df32cdbf04
Trang Nguyen <ntttrang2130@gmail.com>
2023-12-12 22:44:06 -0500

    add new post in db

--- social-network-react-sql/api/controllers/post.js
+++ social-network-react-sql/api/controllers/post.js
@@ -1,5 +1,7 @@
 import { db } from "../connect.js"
 import jwt from "jsonwebtoken";
+import moment from "moment/moment.js";
+
 export const getPosts = (req, res) => {
   const userId = req.query.userId;
   const token = req.cookies.accessToken;
@@ -23,6 +25,29 @@ export const getPosts = (req, res) => {
     db.query(q, values, (err, data) => {
       if (err) return res.status(500).json(err);
       return res.status(200).json(data);
+    });
+  });
+};
+
+export const addPost = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q = "INSERT INTO posts (`desc`, `img`, `createdAt`, `userId`) VALUES (?)";
+
+    const values = [
+      req.body.desc,
+      req.body.img,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+      userInfo.id
+    ]
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Post has been created ");
     });
   });
 };
