65f25eac014e5febd447603c1b8be7783899e084
Trang Nguyen <ntttrang2130@gmail.com>
2023-12-13 01:00:38 -0500

    add multer for file handler

--- social-network-react-sql/api/index.js
+++ social-network-react-sql/api/index.js
@@ -8,6 +8,7 @@ import postRoutes from "./routes/posts.js"
 import commentRoutes from "./routes/comments.js"
 import likeRoutes from "./routes/likes.js"
 import cors from "cors"
+import multer from "multer"
 import cookieParser from "cookie-parser"
 
 // middleware
@@ -20,6 +21,23 @@ app.use(cors({
   origin: "http://localhost:3000"
 }))
 app.use(cookieParser())
+
+// Multer for file handler
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../client/public/upload')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage })
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  const file = req.file
+  res.status(200).json(file.filename)
+})
 
 app.use("/api/auth", authRoutes)
 app.use("/api/posts", postRoutes)
